813f1fb75b18f38757e08c8c7f6735be822ae69c
Uriel Blanco <uriel.blanco@contractors.roche.com>
2024-03-13 13:23:13 +0100

    feat: change scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello from typescript');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => {
+    res.json({ message: 'Hello from express!' });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
