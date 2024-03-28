2120d3b012312b81bb95e088269a688de0cd95cd
Uriel Blanco <uriel.blanco@contractors.roche.com>
2024-03-28 20:13:00 +0100

    feat: add mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
+
+app.use(
+  cors({
+    credentials: true
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
   res.json({ message: 'Hello from express!' });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import 'dotenv/config'
-import app from './app'
+import 'dotenv/config';
+import app from './app';
+import http from 'http';
+import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Api running at http://localhost:${PORT}`)
-})
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Api running at http://localhost:${PORT}`);
+});
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ylp5fii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.error(error));
